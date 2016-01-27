65cb31f860914fad816d116e523d2ef4243ec6d9
hfter789 <hfter789@yahoo-inc.com>
2016-01-27 12:28:34 -0800

    update react-router example to use react-router 1.0

--- examples/react-router/server.js
+++ examples/react-router/server.js
@@ -12,39 +12,49 @@ var React = require('react');
 var app = require('./app');
 var HtmlComponent = React.createFactory(require('./components/Html'));
 var FluxibleComponent = require('fluxible-addons-react/FluxibleComponent');
-var createElement = require('fluxible-addons-react/createElementWithContext');
-var Router = require('react-router');
+var router = require('react-router');
+var match = router.match;
+var RoutingContext = router.RoutingContext;
 
 var server = express();
 server.use('/public', express['static'](__dirname + '/build'));
 
 server.use(function (req, res, next) {
-    var context = app.createContext();
 
-    debug('Executing navigate action');
-    Router.run(app.getComponent(), req.path, function (Handler, state) {
-        context.executeAction(navigateAction, state, function () {
-            debug('Exposing context state');
-            var exposed = 'window.App=' + serialize(app.dehydrate(context)) + ';';
+	debug('Executing navigate action');
+	match({
+		routes:app.getComponent(),
+		location: req.url
+	}, function (error, redirectLocation, renderProps) {
+		if (error) {
+			res.status(500).send(error.message);
+		} else if (redirectLocation) {
+			res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+		} else if (renderProps) {
+			var context = app.createContext();
+			context.executeAction(navigateAction, {path: req.url}, function () {
+				debug('Exposing context state');
+				var exposed = 'window.App=' + serialize(app.dehydrate(context)) + ';';
+				var Component = React.createFactory(RoutingContext);
+				renderProps.context = context.getComponentContext();
+				var markupElement = React.createElement(
+	                FluxibleComponent,
+	                { context: context.getComponentContext() },
+	                Component(renderProps)
+	            );
+			    var html = React.renderToStaticMarkup(HtmlComponent({
+			        context: context.getComponentContext(),
+			        state: exposed,
+			        markup: markupElement
+			    }));
 
-            debug('Rendering Application component into html');
-            var Component = React.createFactory(Handler);
-            var html = React.renderToStaticMarkup(HtmlComponent({
-                context: context.getComponentContext(),
-                state: exposed,
-                markup: React.renderToString(
-                    React.createElement(
-                        FluxibleComponent,
-                        { context: context.getComponentContext() },
-                        Component()
-                    )
-                )
-            }));
-
-            debug('Sending markup');
-            res.send(html);
-        });
-    });
+				debug('Sending markup');
+				res.status(200).send(html);
+			});
+		} else {
+			next();
+		}
+	})
 });
 
 var port = process.env.PORT || 3000;
